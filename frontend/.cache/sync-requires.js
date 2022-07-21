eb1bd76fc28cab79c76175cf868901c5971ea1f4
Leonardo Silva Pavão <leonardosilvapavao@MacBook-Air-de-Leonardo.local>
2022-07-21 22:36:55 +0200

    uptaded json frontend

--- frontend/.cache/sync-requires.js
+++ frontend/.cache/sync-requires.js
@@ -9,6 +9,8 @@ exports.components = {
   "component---src-pages-404-js": hot(preferDefault(require("/Users/leonardosilvapavao/Desktop/e-commerce-project/frontend/src/pages/404.js"))),
   "component---src-pages-account-js": hot(preferDefault(require("/Users/leonardosilvapavao/Desktop/e-commerce-project/frontend/src/pages/account.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/Users/leonardosilvapavao/Desktop/e-commerce-project/frontend/src/pages/contact.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/leonardosilvapavao/Desktop/e-commerce-project/frontend/src/pages/index.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/leonardosilvapavao/Desktop/e-commerce-project/frontend/src/pages/index.js"))),
+  "component---src-templates-product-detail-js": hot(preferDefault(require("/Users/leonardosilvapavao/Desktop/e-commerce-project/frontend/src/templates/ProductDetail.js"))),
+  "component---src-templates-product-list-js": hot(preferDefault(require("/Users/leonardosilvapavao/Desktop/e-commerce-project/frontend/src/templates/ProductList.js")))
 }
 
