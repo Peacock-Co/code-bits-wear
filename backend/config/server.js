37c4e748c09597f91751f5fef035185ac23bb2b9
Leonardo Silva Pavão <leonardosilvapavao@MacBook-Air-de-Leonardo.local>
2022-07-21 18:42:34 +0200

    fix strapi content type

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '95e0a493124b687f236dedf36afd2068'),
+      secret: env("ADMIN_JWT_SECRET", "95e0a493124b687f236dedf36afd2068"),
     },
   },
 });
